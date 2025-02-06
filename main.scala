e8874b99de73a200c40e8cf8431071ccac727b5f
Rajeev Suravajhula <rajeev.suravajhula@gmail.com>
2025-02-06 01:56:16 -0500

    Adding tests

---
+++ main.scala
@@ -0,0 +1,16 @@
+object main extends App {
+
+    def main(): Unit = {
+        val c = new classNew
+        val d = c.test
+        println(d)
+    }
+}
+
+class classNew{
+    def test: String = {
+        println("kjgvhsfjkghkjs")
+        val a = "lfkgjsdfk"
+        a
+    }
+}
